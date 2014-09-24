3339536017ef6a4b0f49b2e73b130785d3abcb01
Staffan Ankardal <fjafjan4@gmail.com>
2014-09-24 16:50:52 +0200

    Added bounces and edge checking
    Also fixed the position of the ball to be centered. Added min/max pos
    variables but did not actually implement them.

--- game/GameScreen.java
+++ game/GameScreen.java
@@ -34,11 +34,15 @@ public class GameScreen extends Screen {
 	int ballSize;
 	double ballX, ballY;
 	double ballVX, ballVY;
+	double minXPos, maxXPos, minYPos, maxYPos;
+	double bounceCoef;
 	int gameHeight, gameWidth;
 	
 	public GameScreen(Game game){
 		super(game);
 		ballSize = 100;
+		bounceCoef = 0.7;
+		livesleft = 5;
 		gameHeight = game.getGraphics().getHeight();
 		gameWidth =game.getGraphics().getWidth();
 		
@@ -90,30 +94,58 @@ public class GameScreen extends Screen {
 					double xDiff = ballX - event.x;
 					double yDiff = ballY - event.y;
 					// f = x-xk/|x-xk|
-					double xForce = xDiff / Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
-					double yForce = yDiff / Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
+					double xForce = 10 * xDiff / Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
+					double yForce = 10 * yDiff / Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
 					// Change the 0.01 constant to be weight, make sure that the directons
 					// are actually correct... just try it out. 
 					ballVX += xForce;
 					ballVY += yForce;
 					Log.w("Debuggin", "We touch the ball, the resulting force is " + xForce + " " + yForce);
-
 				}
-				
-				// atm this is the only thing we care about?
 			}
 		}
-//		Log.w("Debuggin", "Our ball is at " + ballY);
-
-//		ballVY += 0.1; // Some kind of gravity. 
-		ballX = ballX + ballVX;
-		ballY = ballY + ballVY;
+		updateBall();
 		
-		if(ballY > gameHeight){ // If the ball touches the ground we lose!
-			state = GameState.GameOver;
-		}
+		
+		
+		
 	}
 
+	private void updateBall(){
+		ballVY += 0.1; // Some kind of gravity. 
+		
+		ballX += ballVX;
+		ballY += ballVY;
+		
+		if(ballX < ballSize/2){
+			double overstep = (ballSize/2 - ballX);
+			ballX = ballSize/2 + overstep ;
+			ballVX *= -bounceCoef;
+		}else if(ballX > gameWidth - (ballSize/2)){
+			// overstep represent the amount the ball has went outside the 
+			// game area.
+			double overstep = (ballX - gameWidth + (ballSize/2));
+			ballX = gameWidth - (ballSize/2) - overstep;
+			ballVX *= -bounceCoef;
+		}
+
+		if(ballY < ballSize/2){
+			double overstep = (ballSize/2 - ballY);
+			ballY = ballSize/2 + overstep;
+			ballVY *= -bounceCoef;
+		
+		}else if(ballY > gameHeight - (ballSize/2)){
+			//Log.w("Debuggin", "We are bouncing, position pre bounce is " + ballY);
+			double overstep = (ballY - gameHeight + (ballSize/2));
+			ballY = gameHeight- (ballSize/2) - overstep;
+			//Log.w("Debuggin", "We are bouncing, position pre bounce is " + ballY);
+			ballVY *= -bounceCoef;
+			livesleft -= 1;
+			if (livesleft == 0)
+				state = GameState.GameOver;
+		}		
+	}
+	
 	private boolean inBall(int x, int y){
 		// should change this to a for loop when multiple balls are implemented. 
 		// I should replace ball with "spheroid game object" for the sake of everyone involved. 
@@ -167,17 +199,14 @@ public class GameScreen extends Screen {
 	private void drawRunningUI() {
 		Graphics g = game.getGraphics();
 		g.clearScreen(0);
-		g.drawImage(Assets.ball, (int) ballX,(int) ballY);
+		g.drawImage(Assets.ball, (int) ballX - (ballSize/2),(int) ballY - (ballSize/2));		
 	}
-
 
     private void drawGameOverUI() {
         Graphics g = game.getGraphics();
         g.drawRect(0, 0, 1281, 801, Color.BLACK);
         g.drawString("GAME OVER.", 640, 300, paint);
     }
-
-
 
 	@Override
 	public void pause() {
